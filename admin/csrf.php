399fba847c26aa807ab9972f6161425ef7f26d77
Tobias Bengfort <tobias.bengfort@posteo.de>
2020-07-30 13:21:34 +0200

    csrf: keep using the same token for 1h (mutliple tabs)

--- admin/csrf.php
+++ admin/csrf.php
@@ -7,11 +7,19 @@ function sign($s)
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-	if (!isset($_POST['csrf_token']) || sign($_POST['csrf_token']) !== $_COOKIE['csrf_token']) {
+	if (!isset($_COOKIE['csrf_token']) || $_POST['csrf_token'] !== sign($_COOKIE['csrf_token'])) {
 		http_response_code(403);
 		die();
 	}
 }
 
-$csrf_token = bin2hex(random_bytes(5));
-setcookie('csrf_token', sign($csrf_token));
+if (!isset($_COOKIE['csrf_token'])) {
+	$_COOKIE['csrf_token'] = bin2hex(random_bytes(5));
+	setcookie('csrf_token', $_COOKIE['csrf_token'], array(
+		'expired' => time() + 60 * 60,
+		'secure' => true,
+		'httponly' => true,
+		'samesite' => 'Strict',
+	));
+}
+$csrf_token = sign($_COOKIE['csrf_token']);
