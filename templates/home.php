cbe1f21947f98842de3673b087685c1d3b42d564
Tobias Bengfort <tobias.bengfort@posteo.de>
2021-01-25 10:08:38 +0100

    cachebust static images

--- templates/home.php
+++ templates/home.php
@@ -6,7 +6,7 @@
 	<?php endif ?>
 	<div class="homeRow-map" property="location" typeof="place">
 		<a href="https://www.openstreetmap.org/node/874357616" property="hasMap" target="_blank">
-			<img src="/static/stadtplan.svg" alt="<?php e($lang['map']) ?>" width="596" height="596" />
+			<img src="<?php cachebust('/static/stadtplan.svg') ?>" alt="<?php e($lang['map']) ?>" width="596" height="596" />
 		</a>
 		<div property="geo" typeof="GeoCoordinates">
 			<meta property="latitude" content="52.5026297" />
--- templates/main.php
+++ templates/main.php
@@ -5,7 +5,7 @@
 	<meta http-equiv="Content-Security-Policy" content="default-src 'self'">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?php e($page['title'])?> &middot; Kontakt- und Beratungsstelle für Flüchtlinge und Migrant_innen e. V.</title>
-	<link href="/static/favicon.ico" rel="icon">
+	<link href="<?php cachebust('/static/favicon.ico') ?>" rel="icon">
 	<link href="<?php cachebust("/static/kub-${lang['dir']}.css") ?>" rel="stylesheet" type="text/css" />
 
 	<?php foreach (get_langs() as $l): ?>
@@ -22,7 +22,7 @@
 	<div id="header-container">
 		<header id="header">
 			<a href="<?php e("$baseurl/${lang['code']}/") ?>" class="brand-link" rel="home">
-				<img alt="KuB" src="/static/logo.svg" width="331" height="100" />
+				<img alt="KuB" src="<?php cachebust('/static/logo.svg') ?>" width="331" height="100" />
 			</a>
 			<div id="header-bottom">
 				<?php echo get_module('header-bottom')['body'] ?>
