d3fa8cf50f843ffe17b5698b0e80a760460adf4e
Tobias Bengfort <tobias.bengfort@posteo.de>
2021-05-21 08:18:35 +0200

    admin: fix js crash on missing textarea

--- admin/static/admin.js
+++ admin/static/admin.js
@@ -22,36 +22,38 @@ window.addEventListener('beforeunload', function(event) {
 	}
 });
 
-var dir = document.querySelector('textarea').dir;
-tinymce.init({
-	selector: 'textarea',
-	menubar: false,
-	contextmenu: false,
-	plugins: 'code image link table lists',
-	toolbar: 'undo redo | formatselect | bold italic link | image table numlist bullist infobox | outdent indent | readmore | code',
-	content_css: '/xi/static/kub-' + dir + '.css',
-	formats: {
-		info: {block: 'aside', classes: 'infobox', wrapper: true},
-		warning: {block: 'div', classes: 'alert', wrapper: true},
-	},
-	block_formats: 'Paragraph=p; Heading 3=h3; Heading 4=h4; Heading 5=h5; Heading 6=h6; Info Box=info; Warning Box=warning',
-	image_dimensions: false,
-	convert_urls: false,
-	entity_encoding: 'raw',
-	directionality: dir,
-	setup: (editor) => {
-		editor.ui.registry.addButton('readmore', {
-			icon: 'horizontal-rule',
-			tooltip: 'read more',
-			onAction: function () {
-				return editor.execCommand('mceInsertContent', false, '<hr class="system-read-more" />');
-			}
-		});
-		editor.on('input', function() {
-			var form = editor.getElement().closest('form');
-			if (form && !unsavedForms.includes(form)) {
-				unsavedForms.push(form);
-			}
-		});
-	}
-});
+var textarea = document.querySelector('textarea');
+if (textarea) {
+	tinymce.init({
+		target: textarea,
+		menubar: false,
+		contextmenu: false,
+		plugins: 'code image link table lists',
+		toolbar: 'undo redo | formatselect | bold italic link | image table numlist bullist infobox | outdent indent | readmore | code',
+		content_css: '/xi/static/kub-' + textarea.dir + '.css',
+		formats: {
+			info: {block: 'aside', classes: 'infobox', wrapper: true},
+			warning: {block: 'div', classes: 'alert', wrapper: true},
+		},
+		block_formats: 'Paragraph=p; Heading 3=h3; Heading 4=h4; Heading 5=h5; Heading 6=h6; Info Box=info; Warning Box=warning',
+		image_dimensions: false,
+		convert_urls: false,
+		entity_encoding: 'raw',
+		directionality: textarea.dir,
+		setup: (editor) => {
+			editor.ui.registry.addButton('readmore', {
+				icon: 'horizontal-rule',
+				tooltip: 'read more',
+				onAction: function () {
+					return editor.execCommand('mceInsertContent', false, '<hr class="system-read-more" />');
+				}
+			});
+			editor.on('input', function() {
+				var form = editor.getElement().closest('form');
+				if (form && !unsavedForms.includes(form)) {
+					unsavedForms.push(form);
+				}
+			});
+		}
+	});
+}
