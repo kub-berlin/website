f9895f65234c59db8ecd68460d2ed709ee2bc4e8
Tobias Bengfort <tobias.bengfort@posteo.de>
2022-11-04 08:33:56 +0100

    refactor accordion.js

--- static/accordion.js
+++ static/accordion.js
@@ -1,20 +1,5 @@
 // wrap sections (starting with <h3>) in <details> elements.
 
-var prependChild = function(par, child) {
-    if (par.childNodes.length === 0) {
-        par.appendChild(child);
-    } else {
-        par.insertBefore(child, par.childNodes[0]);
-    }
-};
-
-var wrap = function(node, wrapName) {
-    var wrapper = document.createElement(wrapName);
-    node.parentNode.insertBefore(wrapper, node);
-    wrapper.appendChild(node);
-    return wrapper;
-};
-
 var body = document.querySelector('.accordion');
 
 if (body) {
@@ -22,33 +7,29 @@ if (body) {
     if (untranslated) {
         body = untranslated;
     }
-}
 
-if (body) {
-    var sections = [document.createElement('details')];
+    var details = document.createElement('details');
 
     // iterate backwards in order to not change the node indices
     var i = body.childNodes.length;
     while (i--) {
         var node = body.childNodes[i];
         if (node.nodeType === Node.ELEMENT_NODE && node.nodeName === 'H3') {
-            var summary = wrap(node, 'summary');
+            var summary = document.createElement('summary');
             summary.className = 'accordion-summary';
-            prependChild(sections[0], summary);
-            sections.unshift(document.createElement('details'));
+            summary.append(node);
+            details.prepend(summary);
+
+            body.append(details);
+            details = document.createElement('details');
         } else {
-            prependChild(sections[0], node);
+            details.prepend(node);
         }
     }
 
-    var j = sections.length;
-    while (--j) {
-        prependChild(body, sections[j]);
-    }
-
     // the first section does not have a heading, so it is unwrapped
-    var k = sections[0].childNodes.length;
+    var k = details.childNodes.length;
     while (k--) {
-        prependChild(body, sections[0].childNodes[k]);
+        body.prepend(details.childNodes[k]);
     }
 }
