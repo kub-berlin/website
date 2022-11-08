af6ed7320863358eb5b7c4c1806868d433f81758
Tobias Bengfort <tobias.bengfort@posteo.de>
2022-11-08 07:05:41 +0100

    accordion: iterate forward

--- static/accordion.js
+++ static/accordion.js
@@ -8,29 +8,20 @@ if (body) {
         body = untranslated;
     }
 
-    var details = document.createElement('details');
-
-    // iterate backwards in order to not change the node indices
-    var i = body.childNodes.length;
-    while (i--) {
+    var details = null;
+    for (var i = 0; i < body.childNodes.length; i++) {
         var node = body.childNodes[i];
         if (node.nodeType === Node.ELEMENT_NODE && node.nodeName === 'H3') {
+            details = document.createElement('details');
             node.before(details);
 
             var summary = document.createElement('summary');
             summary.className = 'accordion-summary';
             summary.append(node);
-            details.prepend(summary);
-
-            details = document.createElement('details');
-        } else {
-            details.prepend(node);
+            details.append(summary);
+        } else if (details) {
+            details.append(node);
+            i -= 1;
         }
-    }
-
-    // the first section does not have a heading, so it is unwrapped
-    var k = details.childNodes.length;
-    while (k--) {
-        body.prepend(details.childNodes[k]);
     }
 }
