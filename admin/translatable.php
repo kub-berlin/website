35f62fe9db39a490d5c8e4c772ad6395c79f62f9
Tobias Bengfort <tobias.bengfort@posteo.de>
2022-12-29 12:45:42 +0100

    allow to download content in format suitable for transifex

---
+++ admin/translatable.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+// this view allows to download content in a format similar to what we use for
+// transifex.
+
+include_once('../datasource.php');
+
+$translation = get_translation($_GET['page'], $_GET['lang']);
+
+if (!$translation) {
+	http_response_code(404);
+	echo "404 Not Found";
+	exit(1);
+}
+?>
+<div>
+<h1><?php e($translation['title']) ?></h1>
+<?php echo $translation['body'] . "\n" ?>
+</div>
