f784ba321fa001c03890dfcfce1a5e3575077941
Tobias Bengfort <tobias.bengfort@posteo.de>
2023-08-21 07:07:32 +0200

    set content type for sitemap

--- sitemap.php
+++ sitemap.php
@@ -13,6 +13,8 @@ function render_sitemap($parent_id, $parent_url)
 	}
 }
 
+header('Content-Type: application/xml; charset=utf-8');
+
 ?><?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 <?php render_sitemap(1, "https://kub-berlin.org$baseurl/$fallback_lang_code/") ?>
